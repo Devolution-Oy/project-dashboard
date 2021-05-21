e473513baaa70b3f0ef2fb3406811a6ddfdf0977
Killian <doiteau.killian@gmail.com>
2021-05-21 10:38:23 +0300

    Test pr with workflow 2

--- src/Pages/Landing/Landing.test.js
+++ src/Pages/Landing/Landing.test.js
@@ -3,6 +3,6 @@ import Landing from './Landing';
 
 test('renders learn react link', () => {
   render(<Landing />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Landing/i);
   expect(linkElement).toBeInTheDocument();
 });
