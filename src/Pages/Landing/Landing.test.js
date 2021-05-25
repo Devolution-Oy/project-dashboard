2799a15a7014870f30c21e1a31b05b8b4cccd045
Killian <doiteau.killian@gmail.com>
2021-05-25 11:09:48 +0300

    Fire base deploy modif

--- src/Pages/Landing/Landing.test.js
+++ src/Pages/Landing/Landing.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import Landing from './Landing';
 
 test('renders learn react link', () => {
-  render(<Landing />);
-  const linkElement = screen.getByText(/Landing/i);
+  render(<Landing/>);
+  const linkElement = screen.getByText(/Landing page/i);
   expect(linkElement).toBeInTheDocument();
 });
