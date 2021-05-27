b491db9838d22de7a3fac8a24c764cb428bb3bf9
RonanCadot <ronan.cadot@gmail.com>
2021-05-27 09:31:41 +0300

    Footer5

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,6 +1,3 @@
 export const LANDING = '/';
 export const MAIN = '/main';
 export const PROJECT = '/project';
-export const ADMIN = '/admin';
-export const USER = '/user';
-export const TEST_LOGIN = '/test_login';
