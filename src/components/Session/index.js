d6590ef34ab4747126dd760517e8632e584c25ad
Killian <doiteau.killian@gmail.com>
2021-05-27 14:02:32 +0300

    AuthContext 2

--- src/components/Session/index.js
+++ src/components/Session/index.js
@@ -1,6 +1,4 @@
 import AuthContext from './context';
 import withAuthentication from './withAuthentication';
-import SignOutButton from './SignOut';
-import withAuthorization from './withAuthorization';
 
-export { AuthContext, withAuthentication, SignOutButton, withAuthorization };
+export { AuthContext, withAuthentication};
