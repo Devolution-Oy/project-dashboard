2e17877e77bb2ad4f6114b67b7709cdd4b8af322
RonanCadot <ronan.cadot@gmail.com>
2021-05-27 10:17:06 +0300

    Footer8

--- src/components/Footer/Footer.test.js
+++ src/components/Footer/Footer.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import Footer from './Footer';
 
 test('renders learn react link', () => {
-    render(<Footer/>);
-    const linkElement = screen.getByText(/Find us/i);
-    expect(linkElement).toBeInTheDocument();
+  render(<Footer/>);
+  const linkElement = screen.getByText(/Find us/i);
+  expect(linkElement).toBeInTheDocument();
 });
