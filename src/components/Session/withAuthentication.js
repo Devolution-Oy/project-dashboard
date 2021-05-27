d82e46a08103fc96ff4a0c9f76a541c9e45ad784
Killian <doiteau.killian@gmail.com>
2021-05-27 11:32:13 +0300

    AuthContext 1

---
+++ src/components/Session/withAuthentication.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import AuthContext from './context';
+import { withFirebase } from '../Firebase';
+
+const withAuthentication = Component => {
+  class WithAuthentication extends React.Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        authUser: null,
+      };
+    }
+
+    render() {
+      return (
+        <AuthContext.Provider value={this.state.authUser}>
+          <Component {...this.props} />
+        </AuthContext.Provider>
+      );
+    }
+  }
+  
+  WithAuthentication.propTypes = {
+    firebase: PropTypes.object
+  };
+  return withFirebase(WithAuthentication);
+};
+
+export default withAuthentication;
