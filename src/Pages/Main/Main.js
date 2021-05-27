2f9a0d703f1ed40f8b69db6c0a2c81ee3fb9c9de
RonanCadot <ronan.cadot@gmail.com>
2021-05-27 10:46:12 +0300

    Header

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Footer from '../../components/Footer/Footer';
+import Header from '../../components/Header/Header';
+
 function Main() {
   return (
     <div>
       <main className='main-content'>
+        <Header />
         <p>Main page</p>
         <Footer />
       </main>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css';
+function Header() {
+  return (
+    <header>
+      <img src="https://devolution.fi/wp-content/uploads/2020/09/logo-devolution.svg" width= "813px"
+        height="93px" alt="LogoDevolution" id="logo"></img>
+    </header>
+  );
+}
+export default Header;
