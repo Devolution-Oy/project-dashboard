293de3989b6a9d7aa2071cce6e651d0eae8f76ed
RonanCadot <84316548+RonanCadot@users.noreply.github.com>
2021-05-27 10:50:15 +0300

    Header (#48)

--- src/Pages/Landing/Landing.js
+++ src/Pages/Landing/Landing.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Login from '../../components/Login/Login';
-
+import Header from '../../components/Header/Header';
 function Landing() {
   return (
     <div>
       <main className='main-content'>
+        <Header />
         <p>Landing page</p>
         <Login />
       </main>
--- src/Pages/Project/Project.js
+++ src/Pages/Project/Project.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Footer from '../../components/Footer/Footer';
+import Header from '../../components/Header/Header';
 function Project() {
   return (
     <div>
       <main className='main-content'>
+        <Header />
         <p>Project page</p>
         <Footer />
       </main>
