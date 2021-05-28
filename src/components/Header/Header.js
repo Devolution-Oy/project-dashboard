4af811a8b87f1ef11a11fe6eff3eb9c87587a401
RonanCadot <ronan.cadot@gmail.com>
2021-05-28 09:36:16 +0300

    Header responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import './Header.css';
 function Header() {
   return (
     <header>
-      <img src="https://devolution.fi/wp-content/uploads/2020/09/logo-devolution.svg" width= "813px"
+      <img src="https://devolution.fi/wp-content/uploads/2020/09/logo-devolution.svg" width= "90%"
         height="93px" alt="LogoDevolution"></img>
     </header>
   );
