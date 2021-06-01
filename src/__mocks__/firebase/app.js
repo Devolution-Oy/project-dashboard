2625487ed231684aa2346c3ef8e004cf5eeb50a0
Mikko Kurkela <mikko.sahkoposti@gmail.com>
2021-06-01 12:26:14 +0300

    Firebase mock (#64)
    Co-authored-by: Mikko Kurkela <mikko.kurkela@devolution.fi>

---
+++ src/__mocks__/firebase/app.js
@@ -0,0 +1,33 @@
+var userChangedCb = null;
+
+const authObject = {
+  signInWithEmailAndPassword: async (email, pw) => {
+    if (email && pw)
+      return Promise.resolve('Hello');
+    else {
+      const error = {
+        code: 'auth/invalid-email'
+      };
+      return Promise.reject(error);
+    }
+  },
+  onAuthStateChanged: jest.fn(cb => {
+    cb(null);
+  }),
+  onUserChanged: jest.fn((cb) => {
+    userChangedCb = cb;
+  }),
+  setAuthUser: jest.fn((user) => {
+    userChangedCb(user);
+  })
+};
+
+
+const app = {
+  initializeApp: jest.fn(),
+  auth: () => {
+    return authObject;
+  }
+};
+
+export default app;
