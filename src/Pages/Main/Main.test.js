7bc70aeae9910f4859a0b413c65f36dac8efea77
RonanCadot <ronan.cadot@gmail.com>
2021-06-03 10:18:39 +0300

    DesignList5

--- src/Pages/Main/Main.test.js
+++ src/Pages/Main/Main.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import Main from './Main';
 
-test('renders learn ListItem', () => {
+test('renders ListItem', () => {
   render(<Main/>);
   const DesignList = screen.getByTestId('List');
   expect(DesignList).toBeInTheDocument();
