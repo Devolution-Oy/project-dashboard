032c6d34492affc7a5fd815a8f16a9168df5d651
Mikko Kurkela <mikko.kurkela@devolution.fi>
2021-06-04 14:16:32 +0300

    Fix app test file

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,20 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import Firebase, { FirebaseContext } from './components/Firebase';
 import userEvent from '@testing-library/user-event';
 import { validEmail, validPassword} from './constants/testData';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByTestId(/main-content/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+  test('renders learn react link', () => {
+    render(
+      <FirebaseContext.Provider value={new Firebase()}>
+        <App />
+      </FirebaseContext.Provider>);
+    const linkElement = screen.getByTestId(/main-content/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 
-describe('Login Component', () => {
   it('Changes view to main after succesfull login', async () => {
     render(
       <FirebaseContext.Provider value={new Firebase()}>
