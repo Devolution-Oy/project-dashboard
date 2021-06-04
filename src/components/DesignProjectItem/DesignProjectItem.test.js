f19ec0c82c79f77fb941ab6e4e3ee17da6c362cf
RonanCadot <ronan.cadot@gmail.com>
2021-06-04 13:54:41 +0300

    DesignProjectItem2

--- src/components/DesignProjectItem/DesignProjectItem.test.js
+++ src/components/DesignProjectItem/DesignProjectItem.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import DesignProjectItem from './DesignProjectItem';
 
+window.matchMedia = window.matchMedia || function() {
+  return {
+    matches: false,
+    addListener: function() {},
+    removeListener: function() {}
+  };
+};
+
 test('renders learn react link', () => {
   render(<DesignProjectItem/>);
-  const linkElement = screen.getByText(/Well said/i);
+  const linkElement = screen.getByText(/Name/i);
   expect(linkElement).toBeInTheDocument();
 });
